517773d00e7d72e991a823212bd5980d983c0122
Tan Dat Khuu <tandatkhuu2k3@gmail.com>
2025-09-19 19:48:30 +0700

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -3700,7 +3700,7 @@ def create_animation_gif(lang_code, model_id, model_data, validated_params, spee
                         ax.set_xlim(-max_coord, max_coord); ax.set_ylim(-max_coord, max_coord)
                     ax.set_aspect('equal'); ax.axis('off')
                     ax.legend([MplCircle((0,0), 0.1, color='#A52A2A', ec='black', lw=0.5)], [_tr("screen3_legend_model2_cell")], loc='upper right')
-                    ax.set_title(_tr("screen3_model2_anim_plot_title") + f"\nTime: {t_data[frame_idx]:.2f} {time_unit} | cell: {len(model2_cells)}")
+                    ax.set_title(_tr("screen3_model2_anim_plot_title") + f"\nTime: {t_data[frame_idx]:.2f} {time_unit} | {cell}: {len(model2_cells)}")
 
                 elif model_id == 'model3':
                     ended_by_logic = abm_instance.step()
